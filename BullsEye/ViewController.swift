2e8148d0fb0ae161ca8d3d2b6be0f639b3c744bc
Renata Siqueira <rsiqueira@blissapplications.com>
2022-09-22 11:13:57 +0100

    done

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Renata Siqueira on 21/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showAlert(){
+       let alert = UIAlertController(
+        title: "Hello, World!",
+        message: "This is my first app!",
+        preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
+
